5c5b3eb91e9fc75846e596eec45ac459812f0c31
rasitxy <mrdurmus@outlook.com>
2022-11-19 20:06:52 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 
 fn main() {
+    println!("INIT");
     loop {
         let mut line: String = String::new();
 
@@ -10,9 +11,22 @@ fn main() {
 
         let strline: &str = line.as_str().trim();
 
-        let response = match strline {
-            "test" => test("test"),
-            "alphabet" => abc(),
+        let params = strline.split_whitespace();
+
+        let mut param_array: [&str; 2] = ["", ""];
+        let mut param_array_index = 0;
+
+        for param in params {
+            param_array[param_array_index] = param;
+            param_array_index = param_array_index + 1;
+        }
+
+        let command = param_array[0];
+
+        let response = match command {
+            "test" => test_command("test"),
+            "alphabet" => alphabet_command(),
+            "su" => su_command(param_array[1]),
             _ => "COMMAND NOT FOUND.",
         };
 
@@ -20,10 +34,15 @@ fn main() {
     }
 }
 
-fn test(_text: &str) -> &str {
+fn test_command(_text: &str) -> &str {
     "test" as &str
 }
 
-fn abc() -> &'static str {
+fn alphabet_command() -> &'static str {
     "abcdef...."
+}
+
+fn su_command(string: &str) -> &'static str {
+    println!("Switch to the user {}", string);
+    ""
 }
