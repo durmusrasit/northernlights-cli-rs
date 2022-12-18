a9aa3587c4d809c455cc02346441a495bf031a70
durmusrasit <mrdurmus@outlook.com>
2022-12-18 21:30:37 +0300

    Update Alphabet command

--- src/commands.rs
+++ src/commands.rs
@@ -3,7 +3,13 @@ pub fn test_command(_text: &str) -> &str {
 }
 
 pub fn alphabet_command() -> &'static str {
-    "abcdef...."
+    let mut alphabet = String::from("");
+    for c in b'a'..=b'z' {
+        let character = c as char;
+        alphabet.push_str(&character.to_string());
+    }
+    println!("{}", alphabet);
+    ""
 }
 
 pub fn su_command(string: &str) -> &'static str {
